79efff051c3bddcf524ff00acab8e4737880bf3c
Claire Reynaud <claire12.reynaud@laposte.net>
2013-05-05 19:59:10 +0200

    Add a page for my resume

--- claire/urls.py
+++ claire/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', views.home, name='home'),
+    url(r'^resume/$', views.resume, name='resume'),
 )
--- claire/views.py
+++ claire/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     response = render(request, "home.html")
     return response
+
+
+def resume(request):
+    response = render(request, "resume.html")
+    return response
